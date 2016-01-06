07ed77b2e86117dec072ac64ac1dbb257e0d5f8c
Eugene Siow <kyo116@gmail.com>
2016-01-06 17:57:52 +0000

    Add sparql2stream translation

--- src/main/java/uk/ac/soton/ldanalytics/uniote/node/QueryServer.java
+++ src/main/java/uk/ac/soton/ldanalytics/uniote/node/QueryServer.java
@@ -1,8 +1,18 @@
 package uk.ac.soton.ldanalytics.uniote.node;
 
-import java.util.StringTokenizer;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.jena.query.Query;
+import org.apache.jena.sparql.algebra.Algebra;
+import org.apache.jena.sparql.algebra.Op;
+import org.apache.jena.sparql.algebra.OpWalker;
 import org.zeromq.ZMQ;
+
+import uk.ac.soton.ldanalytics.sparql2sql.model.RdfTableMapping;
+import uk.ac.soton.ldanalytics.sparql2sql.model.SparqlOpVisitor;
+import uk.ac.soton.ldanalytics.sparql2sql.util.SQLFormatter;
+import uk.ac.soton.ldanalytics.sparql2stream.parser.StreamQueryFactory;
 
 public class QueryServer {
 
@@ -12,17 +22,36 @@ public class QueryServer {
         ZMQ.Socket subscriber = context.socket(ZMQ.SUB);
         subscriber.connect("tcp://localhost:5600");
 
-        //  Subscribe on everything
+        //  Subscribe on datastreams registered
         subscriber.subscribe("http://www.cwi.nl/SRBench/observations".getBytes());
+        
+        RdfTableMapping mapping = new RdfTableMapping();
+    	mapping.loadMapping("/Users/eugene/Downloads/knoesis_observations_ike_map_meta/HP001.nt");
+		Map<String,String> streamCatalog = new HashMap<String,String>();
+		streamCatalog.put("_HP001", "http://www.cwi.nl/SRBench/observations");
 
-        //  Process 100 updates
-        int update_nbr;
-        long total_temp = 0;
-        for (update_nbr = 0; update_nbr < 100; update_nbr++) {
+        //  wait for messages
+        while (!Thread.currentThread ().isInterrupted ()) {        	
             //  Use trim to remove the tailing '0' character
+            String sub = subscriber.recvStr();
+            String qid = subscriber.recvStr();
             String add = subscriber.recvStr();
-            String string = subscriber.recvStr();
-            System.out.println(string);
+            String queryStr = subscriber.recvStr();
+            long queryHash = queryStr.hashCode();
+//            System.out.println(qid + ":" + add + ":" + queryStr + ":" + queryHash);
+            
+            Query query = StreamQueryFactory.create(queryStr);
+    		Op op = Algebra.compile(query);
+//    		System.out.println(op);
+    		
+    		SparqlOpVisitor v = new SparqlOpVisitor();
+    		v.useMapping(mapping);
+    		v.setNamedGraphs(query.getNamedGraphURIs());
+    		v.setStreamCatalog(streamCatalog);
+    		OpWalker.walk(op,v);
+    		SQLFormatter formatter = new SQLFormatter();
+    		
+    		System.out.println(formatter.format(v.getSQL()));
         }
 
         subscriber.close();
